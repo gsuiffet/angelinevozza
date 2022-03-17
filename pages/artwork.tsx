d1a28d49b8a09f83de64c46325813fa739b3b13d
guillaume.suiffet <guillaume.suiffet@wefight.co>
2022-03-17 19:03:47 +0100

    Use component card in artwork and fix dropdown typo

--- pages/artwork.tsx
+++ pages/artwork.tsx
@@ -8,6 +8,7 @@ import '@szhsin/react-menu/dist/transitions/slide.css';
 import Chevron from "assets/chevron-down.svg";
 import React, {useState, useEffect, useCallback} from "react";
 import Carousel, { Modal, ModalGateway } from "react-images";
+import Card from "../components/Card";
 
 interface TypeIllustrationsByType {
   key: string,
@@ -20,7 +21,7 @@ const Artwork: NextPage = () => {
   const [currentImage, setCurrentImage] = useState<number>(0);
   const [viewerIsOpen, setViewerIsOpen] = useState<boolean>(false);
 
-  const openLightbox = useCallback((event, { photo, index }) => {
+  const openLightbox = useCallback((event, { index }) => {
     setCurrentImage(index);
     setViewerIsOpen(true);
   }, []);
@@ -36,7 +37,6 @@ const Artwork: NextPage = () => {
         const response = await fetch("/api/illustrations");
         const data = await response.json();
         setIllustrationsByType(data);
-        setSelectedCategory(data[0].key);
       })()
     }
   }, [illustrationsByType]);
@@ -59,7 +59,7 @@ const Artwork: NextPage = () => {
       className="text-black flex items-center border border-black px-4 rounded-xl"
     >
       <div className="py-2 flex justify-center">
-        <p className="text-black font-semibold first-letter:capitalize">{selectedCategory}</p></div>
+        <p className="text-black font-semibold first-letter:capitalize">{selectedCategory?.replaceAll("_", " ")}</p></div>
       <Chevron width={12} className="ml-8" fill="#000" />
     </MenuButton>
   );
@@ -67,6 +67,30 @@ const Artwork: NextPage = () => {
   return (
     <MainLayout isFooter={true}>
       <div className="flex flex-col justify-between min-h-screen">
+        {
+          !selectedCategory &&
+          <div className="relative -mt-16">
+            <h2 className={`${selectedCategory ? "mt-20" : "mt-44 md:mt-40"}`}>Mes Artworks</h2>
+            <div className={`${selectedCategory ? "md:mt-32" : ""} px-2 pb-2 md:overflow-y-auto md:absolute flex flex-wrap justify-center w-full`}>
+              {
+                categories.map((category, index) => {
+                  const illustrationValue = illustrationsByType.find(({key}) => key === category)?.value;
+                  if (illustrationValue) {
+                    const illustrationStyle = { backgroundImage: `url(${illustrationValue[0].src}`}
+                    return <Card
+                      key={index}
+                      onClick={() => setSelectedCategory(category)}
+                      additionalContainerStyle="bg-cover"
+                      additionalStyle="flex"
+                      bg={illustrationStyle}
+                      title={category}
+                    />
+                  }
+                })
+              }
+            </div>
+          </div>
+        }
         <ModalGateway>
           {viewerIsOpen && illustrationsToDisplay ? (
             <Modal onClose={closeLightbox}>
@@ -86,7 +110,7 @@ const Artwork: NextPage = () => {
                     return (
                       <MenuItem key={category} value={category}>
                         <div className="py-2 flex">
-                          <p className="text-black first-letter:capitalize">{category}</p>
+                          <p className="text-black first-letter:capitalize">{category.replaceAll("_", " ")}</p>
                         </div>
                       </MenuItem>
                     );
