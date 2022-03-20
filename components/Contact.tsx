bc3cf944d00c0828bbcaebe7c40d0cc446d4ded4
guillaume.suiffet <guillaume.suiffet@wefight.co>
2022-03-20 18:37:28 +0100

    Typo

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return <a href="mailto:angelinevozza@gmail.com">
-      <button className="btn w-fit">Contact moi</button>
+      <button className="btn w-fit">Contactez moi</button>
     </a>
 }
 
