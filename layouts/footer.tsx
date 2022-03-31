d8e662dad579e2b6ae287a2da66416918a1c3f90
guillaume.suiffet <guillaume.suiffet@wefight.co>
2022-03-31 19:18:31 +0200

    Fix footer background

--- layouts/footer.tsx
+++ layouts/footer.tsx
@@ -7,19 +7,21 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="bg-pattern bg-repeat">
-      <div className="section flex space-x-5 items-center justify-between">
-        <div className="flex space-x-5 my-4">
-          <a href={LINKEDIN} target="_blank" rel="noreferrer">
-            <Linkedin height={32} />
-          </a>
-          <a href={INSTAGRAM} target="_blank" rel="noreferrer">
-            <Instagram height={32} />
-          </a>
+    <div className="bg-white">
+      <footer className="bg-pattern bg-repeat">
+        <div className="section flex space-x-5 items-center justify-between">
+          <div className="flex space-x-5 my-4">
+            <a href={LINKEDIN} target="_blank" rel="noreferrer">
+              <Linkedin height={32} />
+            </a>
+            <a href={INSTAGRAM} target="_blank" rel="noreferrer">
+              <Instagram height={32} />
+            </a>
+          </div>
+          <div className="text-black text-tiny">@ AV Créations Graphiques 2022</div>
         </div>
-        <div className="text-black text-tiny">@ AV Créations Graphiques 2022</div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 };
 
